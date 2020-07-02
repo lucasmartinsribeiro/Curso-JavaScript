a1be71e0c94386929d6d2903a08626ce46eb22a8
lucasmartinsribeiro <lucasmartins0498@gmail.com>
2020-07-02 00:09:15 -0300

    Aula 13 e Aula 14

---
+++ Modulo_e/aula13/estruturasDeRepeticao.js
@@ -0,0 +1,15 @@
+//do while
+var cont = 1
+
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+}while(cont <= 6)
+
+// //while
+// var cont = 1
+
+// while(cont <= 6){
+//     console.log(`Passo ${cont}`)
+//     cont++
+// }
